aa26faf5d66c6ae59edfc8f85ed73100e0df2e49
celialab <celialab@outlook.com>
2013-10-07 23:29:43 +0200

    LUCA: camera (vecchi update che non ho mandato xD)

--- Assets/Cameramuvement.cs
+++ Assets/Cameramuvement.cs
@@ -17,6 +17,7 @@ public class Cameramuvement : MonoBehaviour
 		positionX = GameObject.FindGameObjectWithTag("Player").transform.position.x;
 		
 		transform.position= new Vector3(positionX,  21.51884f  , -1f*Mathf.Sqrt( (1- ((positionX*positionX)/1600))*676 ));
+		transform.Rotate(new Vector3(18.5f, Mathf.Atan(-((13*positionX)/(20*Mathf.Sqrt(1600-(positionX*positionX))))), 0));
 		nexshot= Time.time+0.01f;
         //print("ok");
 	}
