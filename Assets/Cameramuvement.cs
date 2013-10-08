20e39b4b3325c9aaa50f632863865ba8640db1d5
celialab <celialab@outlook.com>
2013-10-08 19:41:19 +0200

    LUCA: pulizia

--- Assets/Cameramuvement.cs
+++ Assets/Cameramuvement.cs
@@ -5,7 +5,7 @@ public class Cameramuvement : MonoBehaviour
 {
 	
 	float nexshot = 0.0f;
-	float positionX = 40f;
+	public Transform playerPG;
 	// Use this for initialization
 	void Start () {
 	
@@ -13,11 +13,10 @@ public class Cameramuvement : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () 
-	{		
-		positionX = GameObject.FindGameObjectWithTag("Player").transform.position.x;
-		
-		transform.position= new Vector3(positionX,  21.51884f  , -1f*Mathf.Sqrt( (1- ((positionX*positionX)/1600))*676 ));
-		transform.rotation= Quaternion.Euler(18.5f, Mathf.Atan(-((13*positionX)/(20*Mathf.Sqrt(1600-(positionX*positionX))))), 0);
+	{	
+		float playerPGxPOW=(playerPG.position.x*playerPG.position.x);
+		transform.position= new Vector3(playerPG.position.x,  21.51884f  , -1f*Mathf.Sqrt( (1- (playerPGxPOW/4225))*784 ));
+		//transform.rotation= Quaternion.Euler(18.5f, Mathf.Atan(-((13*playerPG.position.x)/(20*Mathf.Sqrt(1600-playerPGxPOW)))), 0);
 		//transform.Rotate(new Vector3(18.5f, Mathf.Atan(-((13*positionX)/(20*Mathf.Sqrt(1600-(positionX*positionX))))), 0));
 		nexshot= Time.time+0.01f;
         //print("ok");
