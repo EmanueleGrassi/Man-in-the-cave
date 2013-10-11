5c446e2c683c7e5335c3412ca7c7b4ca145dc3b1
celialab <celialab@outlook.com>
2013-10-11 15:13:00 +0200

    Luca: ottimizzazione e camera

--- Assets/Cameramuvement.cs
+++ Assets/Cameramuvement.cs
@@ -18,7 +18,7 @@ public class Cameramuvement : MonoBehaviour
         {
             float playerPGxPOW = (playerPG.position.x * playerPG.position.x);
             transform.position = new Vector3(playerPG.position.x, 21.51884f, -1f * Mathf.Sqrt((1 - (playerPGxPOW / 4225)) * 784));
-            //transform.rotation= Quaternion.Euler(18.5f, Mathf.Atan(-((13*playerPG.position.x)/(20*Mathf.Sqrt(1600-playerPGxPOW)))), 0);
+            transform.rotation= Quaternion.Euler(18.5f, Mathf.Tan(-((28*playerPG.position.x)/(65*Mathf.Sqrt(4225-playerPGxPOW)))) *180/Mathf.PI, 0);
             //transform.Rotate(new Vector3(18.5f, Mathf.Atan(-((13*positionX)/(20*Mathf.Sqrt(1600-(positionX*positionX))))), 0));
             nexshot = Time.time + 0.01f;
             //print("ok");
