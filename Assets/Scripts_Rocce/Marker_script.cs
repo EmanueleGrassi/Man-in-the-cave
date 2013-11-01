3b23d0f65309777f414138d9b9725b625f6775ed
celialab <celialab@outlook.com>
2013-11-01 15:51:58 +0100

    Daniele: audio marker

--- Assets/Scripts_Rocce/Marker_script.cs
+++ Assets/Scripts_Rocce/Marker_script.cs
@@ -4,9 +4,13 @@ using System.Collections;
 public class Marker_script : MonoBehaviour {
 
     float creation;
+    bool play;
+    GameObject pg;
 	// Use this for initialization
 	void Start () {
         creation = Time.time;
+        play = true;
+        pg = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
@@ -14,6 +18,11 @@ public class Marker_script : MonoBehaviour {
         if (Time.time > creation + 3.5f)
         {
             Destroy(gameObject);
+        }
+        if (play && transform.position.x<pg.transform.position.x+10f && transform.position.x>pg.transform.position.x-10f)
+        {
+            audio.Play();
+            play = false;
         }
 	}
 }
