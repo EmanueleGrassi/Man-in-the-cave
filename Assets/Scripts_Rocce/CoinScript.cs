381a7d3f3ab8fd47cbfadb9282735f87dd8ad7ec
celialab <celialab@outlook.com>
2013-11-03 16:51:52 +0100

    Emanuele: modificato il coinscript
    script da assegnare ad ogni coin che scende, l'oggetto ha un valore a
    seconda della tag(oro argento bla bla) e scompare dopo 2 secondi che ha
    toccato terra.

--- Assets/Scripts_Rocce/CoinScript.cs
+++ Assets/Scripts_Rocce/CoinScript.cs
@@ -3,30 +3,50 @@ using System.Collections;
 
 public class CoinScript : MonoBehaviour {
 
-    public float value;
+    public int value;
+    float groundedTime;
+    bool grounded;
 	// Use this for initialization
 	void Start () {
-	    value = 5;
+        switch (gameObject.tag)
+        {
+            case "gold":
+                value = 7;
+                break;
+            case "silver":
+                value = 5;
+                break;
+            case "ruby":
+                value = 8;
+                break;
+            case "zaff":
+                value = 8;
+                break;
+            case "diamond":
+                value = 10;
+                break;
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grounded)
+            if (Time.time > groundedTime + 2)
+                Destroy(gameObject);
+    }
 
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "ground")
         {
             audio.Play();
-            value = 0;
+            groundedTime = Time.time;
+            grounded = true;
         }
         if (col.gameObject.tag == "Player")
         {
-            Vector3 position = transform.position;
-            value += position.y;
-            pg_Script.score += value;
-            Debug.Log("coin: " + value + "  Player score: " + pg_Script.score);
+            CameraScript.data.points += value;
             Destroy(gameObject);
         }
             
