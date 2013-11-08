b0bf2caa267b76b8f28344113c32ea31d76e8b7f
lucaleone <lucaleone@outlook.com>
2013-11-08 01:24:36 +0100

    telecamera

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -96,11 +96,15 @@ public class CameraScript : MonoBehaviour
 			 * z=6
 			 * x=
 			 */
-			transform.position = new Vector3 (playerPG.position.x, Mathf.Sqrt ((1 - (playerPGxPOW / 3000)) * 25f),
-							-1f * Mathf.Sqrt ((1 - (playerPGxPOW / 1500)) * 1f));//y 21.5f			
+			
+			transform.position = new Vector3 (playerPG.position.x, 
+							Mathf.Sqrt ((1 - (playerPGxPOW / 3600)) * 25f),
+				-1*Mathf.Sqrt ((1 - (playerPGxPOW / 1550)) * 25f));	
 			
 			//transform.rotation= Quaternion.Euler(18.5f, Mathf.Tan(-((28*playerPG.position.x)/(65*Mathf.Sqrt(4225-playerPGxPOW)))) *180/Mathf.PI, 0);
-			transform.rotation = Quaternion.Euler (2.9f, Mathf.Tan (-((28 * playerPG.position.x) / (65 * Mathf.Sqrt (4225 - playerPGxPOW)))) * 180 / Mathf.PI, 0);
+			transform.rotation = Quaternion.Euler (2.9f, 
+				Mathf.Tan(-((2 * playerPG.position.x) / (5 * Mathf.Sqrt (2500 - playerPGxPOW)))) * 180 / Mathf.PI,
+				0);
 			//transform.Rotate(new Vector3(18.5f, Mathf.Atan(-((13*positionX)/(20*Mathf.Sqrt(1600-(positionX*positionX))))), 0));
 			nexshot = Time.time + 0.01f;
            
