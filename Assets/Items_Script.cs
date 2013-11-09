89da3223e6fd4be21e3d45d881ce3e6bbf49a0a5
celialab <celialab@outlook.com>
2013-11-09 16:53:11 +0100

    Daniele: inizio  menu item

---
+++ Assets/Items_Script.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Items_Script : MonoBehaviour {
+	
+	float size, margin;
+	int picconi,reborns;
+	public Texture piccone,bengala,coin,blueligth,pinkligth,redligth,greenligth,ranbowligth,reborn;
+	// Use this for initialization
+	void Start () {
+		size = Screen.width / 20;
+        margin = Screen.width / 60;
+		picconi=CameraScript.data.NumberPickaxe;
+		reborns=CameraScript.data.NumberReborn;
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		GUI.skin.label.fontSize = (int)(size * 1.5);
+		GUI.Label(new Rect(margin, margin, size * 5, size * 2 + margin), "Items");
+		GUI.skin.label.fontSize = (int)(size);
+		GUI.Label(new Rect(margin, margin+size*2, size * 10, size * 2 + margin), "Ligths:");
+		GUI.DrawTexture(new Rect(margin*5,margin+size*4,size*2,size*2),blueligth,ScaleMode.StretchToFill);
+		GUI.DrawTexture(new Rect(margin*5+size*3,margin+size*4,size*2,size*2),pinkligth,ScaleMode.StretchToFill);
+		GUI.DrawTexture(new Rect(margin*5+size*6,margin+size*4,size*2,size*2),redligth,ScaleMode.StretchToFill);
+		GUI.DrawTexture(new Rect(margin*5+size*9,margin+size*4,size*2,size*2),greenligth,ScaleMode.StretchToFill);
+		GUI.DrawTexture(new Rect(margin*5+size*12,margin+size*4,size*2,size*2),ranbowligth,ScaleMode.StretchToFill);
+		GUI.Label(new Rect(margin, margin+size*7, size * 10, size * 2 + margin), "Pickaxes:");
+		GUI.DrawTexture(new Rect(margin*5,margin+size*9,size*4,size*4),piccone,ScaleMode.StretchToFill);
+		GUI.Label(new Rect(margin*5+size*5, margin+size*10, size * 10, size * 2 + margin), "x"+picconi);
+		GUI.Label(new Rect(margin, margin+size*14, size * 10, size * 2 + margin), "Reborns:");
+		GUI.DrawTexture(new Rect(margin*5,margin+size*16,size*4,size*4),reborn,ScaleMode.StretchToFill);
+		GUI.Label(new Rect(margin*5+size*5, margin+size*17, size * 10, size * 2 + margin), "x"+reborns);
+	}
+}
