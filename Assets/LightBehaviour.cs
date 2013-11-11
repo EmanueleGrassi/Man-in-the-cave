1af7a57eb5824f1abbe4ad31aaa8eee600744b88
celialab <celialab@outlook.com>
2013-11-12 00:16:34 +0100

    Emanuele: cappellini tranne rainbow

---
+++ Assets/LightBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBehaviour : MonoBehaviour {
+
+    int r, g, b;
+
+	// Use this for initialization
+	void Start () 
+    {
+        if (CameraScript.data.helmet == Helmet.red)
+        {
+            this.light.color = Color.red;
+        }
+        if (CameraScript.data.helmet == Helmet.blue)
+        {
+            this.light.color = Color.blue;
+        }
+        if (CameraScript.data.helmet == Helmet.pink)
+        {
+            this.light.color = Color.magenta;
+        }
+        if (CameraScript.data.helmet == Helmet.green)
+        {
+            this.light.color = Color.green;
+        }
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {    }
+}
