9662f3fcf4e15a4c302778f4431ce58ee28512d6
lucaleone <lucaleone@outlook.com>
2013-11-30 21:44:12 +0100

    animazione

--- Assets/Standard Assets (Mobile)/Scripts/SidescrollControl.js
+++ Assets/Standard Assets (Mobile)/Scripts/SidescrollControl.js
@@ -30,6 +30,15 @@ private var canJump = true;
 var random : int;
 private var nextPlayAudio:float;
 
+
+
+//Animazioni
+private var animator:Animator;
+var pgAnimato:Transform;
+public var DirectionDampTime:float = .25f;
+public var ApplyGravity:boolean = true; 
+//fine animazioni
+
 function Start()
 {
 	// Cache component lookup at startup instead of doing this every frame		
@@ -40,6 +49,12 @@ function Start()
 	var spawn = GameObject.Find( "PlayerSpawn" );
 	if ( spawn )
 		thisTransform.position = spawn.transform.position;
+		
+	//Animazioni
+	animator =pgAnimato.gameObject.GetComponent("Animator");		
+	if(animator.layerCount >= 2)
+		animator.SetLayerWeight(1, 1);
+	//fine animazioni		
 }
 
 function OnEndGame()
@@ -52,78 +67,106 @@ function OnEndGame()
 	this.enabled = false;
 }
 
+
+function OnGUI()
+{
+	GUI.Label (new Rect (100,100, /*moltiplicare la metà delle cifre moneta per height*/ 600f, 300f),
+			"x: "+ v.x+" | y:"+v.y+" | z: "+ v.z+" turnback2:"+turnback2+" turnback:"+turnback);
+        	
+
+}
 function Update()
 {
-	var movement = Vector3.zero;
-	thisTransform.transform.position.z = 7.5;
-	// Apply movement from move joystick
-	if ( moveTouchPad.position.x > 0 )
-		movement = Vector3.right * forwardSpeed * moveTouchPad.position.x;
-	else
-		movement = Vector3.right * backwardSpeed * moveTouchPad.position.x;
-	
-	// Check for jump
-	if ( character.isGrounded )//se sto a terra faccio questo
-	{		
-		var jump = false;
-		var touchPad = jumpTouchPad;
-			
-		if ( !touchPad.IsFingerDown() )
-			canJump = true;
-		
-	 	if ( canJump && touchPad.IsFingerDown() )
-	 	{
-			jump = true;
-			canJump = false;
-	 	}	
-		
-		if ( jump )
-		{
-			// Apply the current movement to launch velocity		
-			velocity = character.velocity;
-			velocity.y = jumpSpeed;	
-		}
+	if (animator)
+	{
+		var stateInfo:AnimatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);	
+		var movement = Vector3.zero;
+		thisTransform.transform.position.z = 7.5;
+		// Apply movement from move joystick
+		if ( moveTouchPad.position.x > 0 )
+			movement = Vector3.right * forwardSpeed * moveTouchPad.position.x;
 		else
-		{
-			/*if (Time.time>nextPlayAudio && moveTouchPad.position.x != 0) 
-			{
-				audio.PlayOneShot(footstep);
-				nextPlayAudio=Time.time+0.35f;
-			}
-			else if(moveTouchPad.position.x == 0)
-			{
-				audio.Stop();
-			}*/
-		}
-	}
-	else// se sono in volo faccio questo
-	{			
-		// Apply gravity to our velocity to diminish it over time
-		velocity.y += Physics.gravity.y * Time.deltaTime;
-				
-		// Adjust additional movement while in-air
-		movement.x *= inAirMultiplier;
-//		movement.z *= inAirMultiplier;
-	}
+			movement = Vector3.right * backwardSpeed * moveTouchPad.position.x;
 		
-	movement += velocity;	
-	movement += Physics.gravity;
-	movement *= Time.deltaTime;
-	
-	// Actually move the character		
-		character.Move( movement );
-	
-	if ( character.isGrounded )		
-		velocity = Vector3.zero;// Remove any persistent velocity after landing	
+		// Check for jump
+		if ( character.isGrounded )//se sto a terra faccio questo
+		{		
+			var jump = false;
+			var touchPad = jumpTouchPad;
+				
+			if ( !touchPad.IsFingerDown() )
+				canJump = true;
+			
+		 	if ( canJump && touchPad.IsFingerDown() )//qui salto
+		 	{
+				jump = true;
+				canJump = false;
+				animator.SetBool("Jump", true);
+		 	}	
+			
+			if ( jump )
+			{
+				// Apply the current movement to launch velocity		
+				velocity = character.velocity;
+				velocity.y = jumpSpeed;	
+			}
+			else
+			{
+				animator.SetBool("Jump", false); 
+				/*if (Time.time>nextPlayAudio && moveTouchPad.position.x != 0) 
+				{
+					audio.PlayOneShot(footstep);
+					nextPlayAudio=Time.time+0.35f;
+				}
+				else if(moveTouchPad.position.x == 0)
+				{
+					audio.Stop();
+				}*/
+			}
+		}
+		else// se sono in volo faccio questo
+		{			
+			// Apply gravity to our velocity to diminish it over time
+			velocity.y += Physics.gravity.y * Time.deltaTime;
+					
+			// Adjust additional movement while in-air
+			movement.x *= inAirMultiplier;
+	//		movement.z *= inAirMultiplier;
+		}
+			
+		movement += velocity;	
+		movement += Physics.gravity;
+		movement *= Time.deltaTime;
+		
+		// Actually move the character		
+			character.Move( movement );
+			
+		//animazione		
+			//float h = Input.GetAxis("Horizontal");
+        	v = movement.normalized;
+        	print("x: "+ v.x);
+        	print("y: "+ movement.normalized.y);
+        	print("z: "+ movement.normalized.z);
+			if(v.x < -0.05 && !turnback)
+			{
+				turnback=true;
+				turnback2=false;
+				transform.rotation= Quaternion.Euler(new Vector3(0,-180,0));
+			}
+			if(v.x > 0.05 && !turnback2)
+			{
+				turnback2=true;
+				turnback=false;
+				transform.rotation= Quaternion.Euler(new Vector3(0,0,0));
+			}
+			animator.SetFloat("Speed", v.x*v.x);
+            animator.SetFloat("Direction", 0, DirectionDampTime, Time.deltaTime);	
+		//fine animazione
+		
+		if ( character.isGrounded )		
+			velocity = Vector3.zero;// Remove any persistent velocity after landing	
+	}
 }
-function OnCollisionEnter(collision:Collision) 
-{
- if(collision.gameObject.tag=="coinGold" || collision.gameObject.tag=="coinSilver")
- 	{
- 	/*if(collision.gameObject.tag=="coinGold") 	
- 		CameraScript.coins =5f;
- 	else
- 		CameraScript.coins=2f;*/
- 	
- 	}
-}
+var  turnback:boolean=false;
+var  turnback2:boolean=true;
+var v:Vector3;
