af35f4c304621ef19eff8cfc200411983cba34e3
lucaleone <lucaleone@outlook.com>
2013-12-11 23:16:35 +0100

    luce pg

--- Assets/Script_FlussoDiGioco/PlayScript.cs
+++ Assets/Script_FlussoDiGioco/PlayScript.cs
@@ -4,58 +4,67 @@ using System.Collections;
 public class PlayScript : MonoBehaviour
 {
     float width, height;
-	public Transform player;
-	public GameObject wplight, pglight;
+    public Transform player;
+    public GameObject wplight, pglight;
     public Transform bengala;
-	bool locked;
+    bool locked;
     bool playJump;
-	int finger;
+    int finger;
     public static int gamePoints;
-	public JoystickC BengalaTouchPad;
-	public enum PlayState
-	{
-		menu,
-		play,
-		pause,
-		result
-	}
-	private static PlayState state;
+    public JoystickC BengalaTouchPad;
+    public enum PlayState
+    {
+        menu,
+        play,
+        pause,
+        result
+    }
+    private static PlayState state;
 
-	public static PlayState State {
-		get { return state; }
-		set {
-			state = value;
-			if (state == PlayState.play) 
-			{
-				Time.timeScale = 1;
-			} 
-			else if (state == PlayState.pause) 
-			{
-				Time.timeScale = 0;
-			} 
-			else if (state == PlayState.menu) 
-			{
-				Time.timeScale = 1;
-			} 
-			else if (state == PlayState.result) 
-			{
-				Time.timeScale = 0;
-			}
-		}
-	}
-	
-	
-	
-	void Start ()
-	{
-		State = PlayState.menu; 
-		//LUCE A SECONDA DELLA PIATTAFORMA
-		if (Application.platform == RuntimePlatform.WP8Player || Application.platform == RuntimePlatform.Android) {
-			wplight.active = true;
-		} else
-			pglight.active = true;
+    public static PlayState State
+    {
+        get { return state; }
+        set
+        {
+            state = value;
+            if (state == PlayState.play)
+            {
+                Time.timeScale = 1;
+            }
+            else if (state == PlayState.pause)
+            {
+                Time.timeScale = 0;
+            }
+            else if (state == PlayState.menu)
+            {
+                Time.timeScale = 1;
+            }
+            else if (state == PlayState.result)
+            {
+                Time.timeScale = 0;
+            }
+        }
+    }
+
+
+
+    void Start()
+    {
+        State = PlayState.menu;
+        //LUCE A SECONDA DELLA PIATTAFORMA
+        if (Application.platform == RuntimePlatform.WP8Player)
+        {
+            pglight.SetActive(false);
+            wplight.SetActive(true);
+        }
+        else
+        {
+            wplight.SetActive(false);
+            pglight.SetActive(true);
+        }
+
         gamePoints = 0;
-	}
+    }
 
     //void OnEndGame ()
     //{
@@ -63,10 +72,10 @@ public class PlayScript : MonoBehaviour
     //    BengalaTouchPad.Disable ();
     //}
 
-	public static bool BenngalaAvailable = true;
+    public static bool BenngalaAvailable = true;
 
-	void Update ()
-	{
+    void Update()
+    {
         if (!audio.isPlaying)
             audio.volume = 1;
         if (CameraScript.data.numBengala == 0)
@@ -81,9 +90,9 @@ public class PlayScript : MonoBehaviour
             Vector3 pos = player.position;
             Instantiate(bengala, new Vector3(pos.x, pos.y + 2, pos.z), Quaternion.identity);
         }
-	}
+    }
 
-	public static void PlayClip(AudioSource audio2,params AudioClip[] list)
+    public static void PlayClip(AudioSource audio2, params AudioClip[] list)
     {
         int random = Random.Range(0, list.Length);
         audio2.PlayOneShot(list[random]);
