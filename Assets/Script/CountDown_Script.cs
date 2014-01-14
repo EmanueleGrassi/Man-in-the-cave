7f7f09d9bcd07aaf429aa59c9a23bb6746eb7ca6
lucaleone <lucaleone@outlook.com>
2014-01-14 17:01:23 +0100

    luci e countdown

---
+++ Assets/Script/CountDown_Script.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountDown_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        numero = 0; 
+        guiText.fontSize = Screen.width / 10;
+	}
+    float CountDownTime = 0f;
+
+    private byte _num = 5;
+    private byte numero
+    {
+        get { return _num; }
+        set 
+        {
+            if (_num != value)
+            {
+                _num = value;
+                if (value == 0)
+                    guiText.text = "survive!";
+                else
+                    guiText.text = value.ToString();
+                //suona
+            }        
+        }
+    }
+
+    void Update()
+    {
+        if (CountDownTime >= 1.5)
+            gameObject.SetActive(false);
+        /*
+        if (CountDownTime >= 1 && CountDownTime < 2)
+            numero = 2;
+        else if (CountDownTime >= 2 && CountDownTime < 3)
+            numero = 1;
+        else if (CountDownTime >= 3 && CountDownTime < 4.5)
+            numero = 0;
+        if (CountDownTime >= 4.5)
+            gameObject.SetActive(false);*/
+
+        CountDownTime += Time.deltaTime;
+    }
+}
