cb535c5c0a49d9c025ec1d1237c70da175633bde
Emanuele Grassi <emanuele.grassi@live.com>
2014-01-22 10:39:20 +0100

    gyro in scores e settings

--- Assets/Scores_script.cs
+++ Assets/Scores_script.cs
@@ -16,6 +16,19 @@ public class Scores_script : MonoBehaviour
     float UnTerzo;
     bool IsScore = true; //se false visualizza achivements
 
+    bool start = true;
+    private Quaternion cameraBase = Quaternion.identity;
+    private Quaternion calibration = Quaternion.identity;
+    private Quaternion baseOrientation = Quaternion.Euler(90, 0, 0);
+    private Quaternion baseOrientationRotationFix = Quaternion.identity;
+    private Quaternion referanceRotation = Quaternion.identity;
+    private readonly Quaternion baseIdentity = Quaternion.Euler(90, 0, 0);
+    private readonly Quaternion landscapeRight = Quaternion.Euler(0, 0, 90);
+    private readonly Quaternion landscapeLeft = Quaternion.Euler(0, 0, -90);
+    private readonly Quaternion upsideDown = Quaternion.Euler(0, 0, 180);
+    Vector3 accel;
+    float filter = 5.0f;
+
     void Start()
     {
         if (CameraScript.data == null)
@@ -149,8 +162,34 @@ public class Scores_script : MonoBehaviour
                 pos.y += touch.deltaPosition.y * scrollparam*2;
             }
         }
-        #endif
-    }
+        if (SystemInfo.supportsGyroscope)
+        {
+            if (start)
+            {
+                Input.gyro.enabled = true;
+                AttachGyro();
+               
+                start = false;
+            }
+
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+            cameraBase * (ConvertRotation(referanceRotation * Input.gyro.attitude) * GetRotFix()), 0.2f);
+        }
+        else if (SystemInfo.supportsAccelerometer)
+        {
+            // filter the jerky acceleration in the variable accel:
+            accel = Vector3.Lerp(accel, Input.acceleration, filter * Time.deltaTime);
+            float x = -((accel.y * 100)); //si muove in alto e basso
+            float DestraSinistra = -90 * accel.x;//si muove a destra e sinistra          
+
+            float Altobasso = (accel.y * 90) + 90;
+            if (accel.z >= 0)
+                Altobasso *= -1;
+            transform.rotation = Quaternion.Euler(Altobasso, DestraSinistra, 0f);
+        }
+#endif
+     }
+    
 
     public static string formatScoreSecond(float a)
     {
@@ -168,4 +207,86 @@ public class Scores_script : MonoBehaviour
 
         return rAdjustedBounds.Contains(screenPos);
     }
+
+    #region GYRO CONTROL
+    private void AttachGyro()
+    {
+        ResetBaseOrientation();
+        UpdateCalibration(true);
+        UpdateCameraBaseRotation(true);
+        RecalculateReferenceRotation();
+    }
+    private void UpdateCameraBaseRotation(bool onlyHorizontal)
+    {
+        if (onlyHorizontal)
+        {
+            var fw = transform.forward;
+            fw.y = 0;
+            if (fw == Vector3.zero)
+            {
+                cameraBase = Quaternion.identity;
+            }
+            else
+            {
+                cameraBase = Quaternion.FromToRotation(Vector3.forward, fw);
+            }
+        }
+        else
+        {
+            cameraBase = transform.rotation;
+        }
+    }
+    private void UpdateCalibration(bool onlyHorizontal)
+    {
+        if (onlyHorizontal)
+        {
+            var fw = (Input.gyro.attitude) * (-Vector3.forward);
+            fw.z = 0;
+            if (fw == Vector3.zero)
+            {
+                calibration = Quaternion.identity;
+            }
+            else
+            {
+                calibration = (Quaternion.FromToRotation(baseOrientationRotationFix * Vector3.up, fw));
+            }
+        }
+        else
+        {
+            calibration = Input.gyro.attitude;
+        }
+    }
+    private static Quaternion ConvertRotation(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+    private Quaternion GetRotFix()
+    {
+#if UNITY_3_5
+		if (Screen.orientation == ScreenOrientation.Portrait)
+			return Quaternion.identity;
+		
+		if (Screen.orientation == ScreenOrientation.LandscapeLeft || Screen.orientation == ScreenOrientation.Landscape)
+			return landscapeLeft;
+				
+		if (Screen.orientation == ScreenOrientation.LandscapeRight)
+			return landscapeRight;
+				
+		if (Screen.orientation == ScreenOrientation.PortraitUpsideDown)
+			return upsideDown;
+		return Quaternion.identity;
+#else
+        return Quaternion.identity;
+#endif
+    }
+    private void ResetBaseOrientation()
+    {
+        baseOrientationRotationFix = GetRotFix();
+        baseOrientation = baseOrientationRotationFix * baseIdentity;
+    }
+    private void RecalculateReferenceRotation()
+    {
+        referanceRotation = Quaternion.Inverse(baseOrientation) * Quaternion.Inverse(calibration);
+    }
+    #endregion
 }
